ba1d0ae5f7fa1285cfb600080d60049743d2d7ee
Sean Kelly <41454023+SKEL91@users.noreply.github.com>
2021-02-09 16:22:17 +0000

    testing how to link java and github

---
+++ StartingPoint/src/FstClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Sean on 09/02/2021
+ * /////1st draft of our group project design
+ *
+ */
+public class FstClass {
+   public static void main(String[] args) {
+   }//main
+}//class 
